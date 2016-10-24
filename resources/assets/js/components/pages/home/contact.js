9439750cdf8d65d9df75c454bfc0ad13081f9758
DaveKun <dawidkunicki88@gmail.com>
2016-10-24 13:09:02 +0200

    dodanie contact form

---
+++ resources/assets/js/components/pages/home/contact.js
@@ -0,0 +1,49 @@
+// TODO make info an IIFE
+// TODO make into Vue.component
+module.exports = {
+    el: '#contact_form', // id of the 'app'
+    data: {
+        name: '',   // data for the name on the form
+        email:'',   // data for the email on the form
+        message:'', // data for the message on the form
+        maxLength: 140 // maximum length of the form message
+    },
+    methods: { // all the actions our app can do
+        isValidName: function () { // TODO what if name is just spaces?
+            var valid = this.name.length > 0;
+            console.log('checking for a valid name: ' + valid);
+            return valid;
+        },
+        isValidEmail: function () { // TODO is a@b a valid email?
+            var valid = this.email.indexOf('@') > 0;
+            console.log('checking for a valid email: ' + valid);
+            return valid;
+        },
+        isValidMessage: function () { // what is message is just spaces?
+            var valid = (this.message.length > 0) &&
+                (this.message.length < this.maxLength);
+            console.log('checking for a valid message: ' + valid);
+            return valid;
+        },
+        checkMessage: function () {
+            // TODO keep the message below maxMessageLength?
+            //      or maybe change the text, background, or counter color?
+        },
+        submitForm: function () {
+            console.log('submitted');
+            // TODO prevent form from submitting if name, email, or message
+            //      are invalid and display message
+            // TODO submit to form processor
+            console.log('submitting message...');
+            this.$http({url: '/someUrl', method: 'POST', data: {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            }}).then(function () {
+                alert('Your form was submitted!');
+            }, function () {
+                alert('Form submission failed');
+            });
+        }
+    }
+};
