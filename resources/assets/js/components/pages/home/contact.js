24bd2c8a9526e87bbba57225890c2843280c0968
DaveKun <dawidkunicki88@gmail.com>
2016-10-27 18:20:12 +0200

    Walidacja dormularza - button disabled działa dzieki metodzie computed

--- resources/assets/js/components/pages/home/contact.js
+++ resources/assets/js/components/pages/home/contact.js
@@ -5,33 +5,36 @@ module.exports = {
     data: {
         name: '',   // data for the name on the form
         email:'',   // data for the email on the form
-        message:'', // data for the message on the form
+        message:'' // data for the message on the form
         //maxLength: 140 // maximum length of the form message
-         errorClass: 'text-danger'
     },
+    computed: {
+         checkIfFormValid: function () {
+           var valid = this.isValidName() && this.isValidEmail() && this.isValidMessage();          
+           console.log("ValiD ", valid);
+          return valid;  
+        }
+        },
     methods: { // all the actions our app can do
         isValidName: function () { 
             var valid = (this.name.length > 0) && (this.name.match(/^\s+$/) === null);
-            console.log('checking for a valid name: ' + valid);
             return valid;
         },
         checkName: function () {
             if (!this.isValidName()) {
-               console.log('Not a valid name');
                $(".form-name").addClass("text-danger");
             }   else {
-               console.log('A valid name');
                $(".form-name").removeClass("text-danger");
             }       
         },
+        
+
         isValidEmail: function () { // TODO is a@b a valid email?
-            var valid = (this.email.indexOf('@') > 0) && (this.email.match(/^\s+$/) === null) ;
-            console.log('checking for a valid email: ' + valid);
+            var valid = (this.email.length > 0) && (this.email.indexOf('@') > 0) && (this.email.match(/^\s+$/) === null) ;
             return valid;
         },
         checkEmail: function () {
             if (!this.isValidEmail()) {
-               console.log('Not a valid e-mail');
                $(".form-email").addClass("text-danger");
             }   else {
                console.log('A valid e-mail!');
@@ -42,25 +45,25 @@ module.exports = {
             var maxLength = 140;
             var valid = (this.message.length > 0) &&
                 (this.message.length < maxLength) && (this.message.match(/^\s+$/) === null);
-            console.log('checking for a valid message: ' + valid);
             return valid;
         },
         checkMessage: function () {
             if (!this.isValidMessage()) {
-               console.log('Not a valid message');
                $(".form-message").addClass("text-danger");
             }   else {
-               console.log('A valid message');
                $(".form-message").removeClass("text-danger");
-            }         
+            }
         },
-        checkIfSubmit: function () {
-          var valid = this.isValidName() && this.isValidEmail() && this.isValidMessage();
-          return valid;  
+        checkIfFormValid: function () {
+        //   var valid = this.isValidName() && this.isValidEmail() && this.isValidMessage();          
+        //   console.log("ValiD ", valid);
+          return this.name != '' && this.email != '';  
         },
         submitForm: function () {
-            if (!this.checkIfSubmit()) {
-               return alert("Please correct the form");
+            if (!this.checkIfFormValid()) {
+                console.log(this.checkIfFormValid());
+               $('#form-error-msg').addClass("show"); 
+               return console.log("Please correct the form");
             }
             console.log('submitted');
             var form = '#contact_form'; // id of the 'app'
@@ -79,4 +82,5 @@ module.exports = {
             });
         }
     }
+    
 };
